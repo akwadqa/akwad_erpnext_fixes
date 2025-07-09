d9254c5e3bc52bc13b43068f47155b318c3d9348
Alaa Hariri <alaa.hariri@outlook.com>
2025-07-09 11:59:23 +0300

    Update __init__.py

--- akwad_erpnext_fixes/__init__.py
+++ akwad_erpnext_fixes/__init__.py
@@ -1,8 +1,1 @@
 __version__ = "0.0.1"
-
-# import extended_erpnext_transactions.native_patches
-
-# from frappe.www import printview
-# from akwad_erpnext_fixes.native_overrides import custom_get_print_style
-
-# printview.get_print_style = custom_get_print_style
